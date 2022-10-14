140b00329bcfca04bbdbff00da50ddd77b6f7aa8
C. W. Chan <cwchan0212@gmail.com>
2022-10-14 19:52:43 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 // https://weather-proxy.freecodecamp.rocks/api/current?lat=53.483959&lon=-2.244644
-// https://codemyui.com/animated-weather-icons-in-css/
+// Weather Icons: https://github.com/Makin-Things/weather-icons
 function App() {
     // const [count, setCount] = React.useState(0);
     const [coord, setCoord] = React.useState([]);
@@ -12,69 +12,77 @@ function App() {
 
     const fetchData = async (coord) => {
         const [lat, lon] = coord;
-        // console.log("data", lat, lon);
-
         const base = "https://weather-proxy.freecodecamp.rocks/api/current";
         const params = new URLSearchParams({
             lat: lat,
             lon: lon,
         });
         const uri = base + "?" + params.toString();
-        // console.log("w len", Object.keys(weather).length)
-        //
+
         if (lat && lon && Object.keys(dummy).length === 0) {
             const res = await fetch(uri);
 
-            console.log("uri", uri);
-            console.log(res.ok);
+
             const data = await res.json();
             setDummy(data);
         }
     };
-    console.log("out", dummy);
+
     React.useEffect(() => {
         if (typeof coord[0] === "undefined") {
             navigator.geolocation.getCurrentPosition((position) => {
                 setCoord([position.coords.latitude, position.coords.longitude]);
-                console.log(coord);
-                // setCount((oldValue) => oldValue + 1);
                 useRef.current += 1;
             });
         }
         fetchData(coord);
-
     }, [coord]);
-    console.log(useRef.current, coord);
-    // if (!useRef.current) {
-    //     fetchData();
-    //     useRef.current = true;
-    // }
-    if (Object.keys(dummy).length > 0) {
-        // console.log("weather.coord.lat", weather.coord.lat)
-    }
 
     const Temperature = () => {
         const temperature = {
             date: new Date(dummy.dt * 1000).toISOString(),
-            temp: unit === "C"? dummy.main.temp : switchToF(dummy.main.temp),
-            feel: unit === "C"? dummy.main.feels_like : switchToF(dummy.main.feels_like),
-            low: unit === "C"? dummy.main.temp_min : switchToF(dummy.main.temp_min),
-            high: unit === "C"? dummy.main.temp_max : switchToF(dummy.main.temp_max),
+            temp: unit === "C" ? dummy.main.temp : switchToF(dummy.main.temp),
+            feel:
+                unit === "C"
+                    ? dummy.main.feels_like
+                    : switchToF(dummy.main.feels_like),
+            low:
+                unit === "C"
+                    ? dummy.main.temp_min
+                    : switchToF(dummy.main.temp_min),
+            high:
+                unit === "C"
+                    ? dummy.main.temp_max
+                    : switchToF(dummy.main.temp_max),
             main: dummy.weather[0].main,
             desc: dummy.weather[0].description,
             icon: dummy.weather[0].icon,
         };
 
         return temperature ? (
-            <div className="temperature">
-                <div className="picture">{temperature.icon}</div>
+            <div className="container">
+                <div className="picture">
+                    <img
+                        className="icon"
+                        src={"img/" + temperature.main.toLowerCase() + ".svg"}
+                        alt={temperature.desc}
+                        loading="lazy"
+                    />
+                </div>
                 <div className="figure1">
-                    Temperature {temperature.temp} °{unit} ( Feel Like:
-                    {temperature.feel}) °{unit}
+                    Temperature {temperature.temp} °{unit}
                 </div>
                 <div className="figure2">
-                    Low {temperature.low} °{unit} --- High {temperature.low} °
-                    {unit}
+                    ( Feel Like {temperature.feel}) °{unit}
+                </div>
+                <div className="figure3">
+                    <span class="low">
+                        Low {temperature.low} °{unit}
+                    </span>{" "}
+                    ---{" "}
+                    <span class="high">
+                        High {temperature.high} °{unit}
+                    </span>
                 </div>
                 <div className="desc">
                     {temperature.main} - {temperature.desc}
@@ -109,11 +117,11 @@ function App() {
     };
 
     const switchToF = (num) => {
-        return ((num * 9) / 5 + 32).toFixed(2)
+        return ((num * 9) / 5 + 32).toFixed(2);
     };
 
     const switchUnit = (unit) => {
-        console.log(unit);
+
         let temp = dummy.main.temp;
         if (unit === "F") {
             setUnit(unit);
@@ -123,29 +131,32 @@ function App() {
             setUnit(unit);
             temp = dummy.main.temp;
         }
-        console.log("je", unit, temp);
+        return temp;
     };
 
     return useRef.current && Object.keys(dummy).length > 0 ? (
         <div className="App">
-            {/* <div className="location">
-                {JSON.stringify(dummy)}
-                <br />
-                <br />
-                {useRef.current},
-
-            </div> */}
-            <Temperature />
+            <div className="banner">
+                <img
+                    className="logo"
+                    src="https://design-style-guide.freecodecamp.org/downloads/fcc_secondary_small.svg"
+                    alt="freeCodeCamp"
+                    loading="lazy"
+                /> => Show Local Weather 
+            </div>
             <Location />
+            <Temperature />
+
             <button
+                class="btn btn-primary"
                 type="button"
                 onClick={() => switchUnit(`${unit === "C" ? "F" : "C"}`)}
             >
-                Switch to {unit === "C" ? "F" : "C"}
+                Switch to °{unit === "C" ? "F" : "C"}
             </button>
         </div>
     ) : (
-        <>Browser not support geolocation</>
+        <div class="App">Browser not support geolocation</div>
     );
 }
 
